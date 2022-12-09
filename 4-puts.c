8418123dc6aff31c675ddd4bbb2784be781fbc34
Mozeedat Adediran <adediranmozeedat@gmail.com>
2022-12-08 16:32:16 -0800

    betty coding style

--- 4-puts.c
+++ 4-puts.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * 
+ * Return: Always 0 (succcess)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
-
+	puts("/"Programming  is like building a multilingual puzzle");
+	return(0);
 }
-		
